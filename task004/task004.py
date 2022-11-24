7d4529e875891818c0fb7c7ba250a1e8c44618bf
Karaton <karaton@inbox.ru>
2022-11-24 15:38:37 +0300

    add task004

---
+++ task004/task004.py
@@ -0,0 +1,10 @@
+print('Введите номер координатной четверти (от 1 до 4)')
+n=int(input())
+if n==1:
+    print('x [0,inf],y [0,inf]')
+elif n==2:
+    print('x [-inf,0],y [0,inf]')
+elif n==3:
+    print('x [-inf,0],y [-inf,0]')
+elif n==4:
+    print('x [0,inf],y [-inf,0]')
